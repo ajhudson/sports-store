16a752bb47d2cc82d084518e35c54beff1a53fde
Andy Hudson <andy.hudson@kaonix.com>
2016-01-08 11:02:54 +0000

    Setting up angular

---
+++ SportsStore/Scripts/ng-controllers/loginController.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    var app = angular.module('loginApp', ['ui.bootstrap']);
+
+    app.factory('loginFactory', ['$http', function ($http) {
+        loginFactory.authenticate = function (usr, pwd) {
+
+        };
+    }]);
+
+    app.controller('LoginController', function () {
+        var lc = this;
+
+        lc.init = function () {
+            lc.username = "the username";
+            lc.password = "the password";
+        }
+
+    });
+})();
